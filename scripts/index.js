6c7126ebc2835ac69dd8985f8a307c7d72e48ae0
Aadityaashutoshsharma <aadityaashutoshsharma@gmail.com>
2023-02-28 00:01:00 +0530

    Created post and postlists pages

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,21 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 
+/* Modal Content Elements */
+var nameEle = document.getElementById("name");
+var userNameEle = document.getElementById("userName");
+var passwordEle = document.getElementById("password");
+var confirmPasswordEle = document.getElementById("confirmPassword");
+
 btn.popUp = () => {
-    modal.style.display = "block";
+    modal.style.display = "flex";
 }
 
 span.newPopUp = () => {
     modal.style.display = "none";
+}
+function getData(){
+    if(nameEle && userNameEle && passwordEle && confirmPasswordEle){
+ console.log(nameEle.value,userNameEle.value,passwordEle.value,confirmPasswordEle.value);
+    }
 }

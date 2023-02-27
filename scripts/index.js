fb4cce09a6f0c6217076ee2c603c20e18521c684
Aadityaashutoshsharma <aadityaashutoshsharma@gmail.com>
2023-02-28 00:56:34 +0530

    Added SignIn Modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,25 @@
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
+var modalSignIn = document.getElementById("myModal-signIn");
+
+// Got the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
+var btnSignIn = document.getElementById("myBtnSignIn");
+
+// Got the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+var spanSignIn = document.getElementsByClassName("closeSignIn")[0];
 
 /* Modal Content Elements */
+
 var nameEle = document.getElementById("name");
 var userNameEle = document.getElementById("userName");
 var passwordEle = document.getElementById("password");
 var confirmPasswordEle = document.getElementById("confirmPassword");
+
+// When the user clicks on the button, open the modal
 
 btn.popUp = () => {
     modal.style.display = "flex";
@@ -21,8 +28,17 @@ btn.popUp = () => {
 span.newPopUp = () => {
     modal.style.display = "none";
 }
-function getData(){
+
+btnSignIn.popUp2 = () => {
+    modalSignIn.style.display = "flex";
+}
+
+spanSignIn.newPopUp2 = () => {
+    modalSignIn.style.display = "none";
+}
+
+function getSignUpData(){
     if(nameEle && userNameEle && passwordEle && confirmPasswordEle){
- console.log(nameEle.value,userNameEle.value,passwordEle.value,confirmPasswordEle.value);
+         console.log(nameEle.value,userNameEle.value,passwordEle.value,confirmPasswordEle.value);
     }
 }
